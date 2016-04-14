8081c54387bf3a2441769fac1bf90a07632a323a
JT Chen <tian5683ke@gmail.com>
2016-04-14 19:51:19 -0400

    Location javabean class

---
+++ src/java/Location.java
@@ -0,0 +1,27 @@
+
+import java.io.Serializable;
+
+
+/**
+ * Location class
+ * relate zip code to city and state
+ * @author Tian
+ */
+public class Location implements Serializable{
+    private int zipCode;
+    private String state;
+    private String city;
+    public Location(){
+        
+    }
+    
+    public void setZipCode(int newZip){ zipCode = newZip;}
+    public int getZipCode(){ return zipCode;}
+    
+    public void setState(String s){ state=s;}
+    public String getState(){ return state;}
+    
+    public void setCity(String c){ city = c;}
+    public String getCity(){ return city;}
+    
+}
