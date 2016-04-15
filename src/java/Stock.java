d2480dec1901b9c797020d5f0a8c7ffef47710eb
JT Chen <tian5683ke@gmail.com>
2016-04-15 00:29:05 -0400

    Stock javabean class

---
+++ src/java/Stock.java
@@ -0,0 +1,39 @@
+
+import java.io.Serializable;
+
+
+/**
+ * Stock
+ * @author Tian
+ */
+public class Stock implements Serializable{
+    private String stockSymbol;
+    private String stockName;
+    private String companyName;
+    private String type;
+    private int numShares;
+    private float pricePerShare;
+    
+    public Stock(){
+    
+    }
+    
+    public void setStockSymbol(String ss){stockSymbol=ss;}
+    public String getStockSymbol(){return stockSymbol;}
+    
+    public void setStockName(String sn){stockName=sn;}
+    public String getStockName(){return stockName;}
+    
+    public void setCompanyName(String cn){companyName=cn;}
+    public String getCompanyName(){return companyName;}
+    
+    public void setType(String t){type=t;}
+    public String getType(){return type;}
+    
+    public void setNumShares(int ns){numShares=ns;}
+    public int getNumShares(){return numShares;}
+    
+    public void setPricePerShare(float pps){pricePerShare=pps;}
+    public float getPricePerShare(){return pricePerShare;}
+    
+}
