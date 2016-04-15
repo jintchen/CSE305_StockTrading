bc47cdf1037cfe6f16ca422f2506d53a4f79c8d3
JT Chen <tian5683ke@gmail.com>
2016-04-14 22:18:42 -0400

    Client javabean class

---
+++ src/java/Client.java
@@ -0,0 +1,25 @@
+
+import java.io.Serializable;
+
+/**
+ * Clinet
+ * @author Tian
+ */
+public class Client extends Person implements Serializable{
+    private String email;
+    private float rating;
+    private int creditCardNumber;
+    
+    public Client(){
+        
+    }
+    
+    public void setEmail(String e){email=e;}
+    public String getEmail(){ return email;}
+    
+    public void setRating(float r){ rating = r;}
+    public float getRating(){ return rating;}
+    
+    public void setCreditCardNumber(int c){creditCardNumber = c;}
+    public int getCreditCardNumber(){return creditCardNumber;}
+}
